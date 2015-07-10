b39c6034284db11863789d513dd37102ef48d190
dsanghavi <darpansanghavi1993@gmail.com>
2015-07-10 20:48:11 +0530

    Working reflected pints being colored accoring to symmetry plane.

--- ui.cpp
+++ ui.cpp
@@ -21,6 +21,8 @@
 #include <ctime>
 #include <cstdlib>
 
+typedef std::tuple<float,float,float> plane;
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Epick;
 typedef CGAL::Triangulation_vertex_base_with_info_2<unsigned,Epick> Vb;
 typedef CGAL::Triangulation_data_structure_2<Vb> Tds;
@@ -58,37 +60,41 @@ struct uiState{
 	boost::shared_ptr<Visualizer> visualizer;
 	pcl::PointCloud<pcl::PointXYZ>::ConstPtr sourceCloud;
 	pcl::PointCloud<pcl::PointXYZ>::Ptr patchCloud;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr reflectCloud;	
 	std::string patchLabel;
-	graph_t *graph;
+	std::string reflectLabel;
 	std::vector<vertex_descriptor> chosenPoints;
-	int firstChosen;
-	int lastChosen;
-	bool pointPickingDone;
-	bool firstPointPicked;
-	bool WASDenabled;
+	bool pointPickingActive;
+	bool patchSelectionDone;
+	float patchRadius;
+	vertex_descriptor activePoint;
 	std::tuple<double,double,double> color;
+	plane p;
 
 	uiState(boost::shared_ptr<Visualizer> _v,
-				pcl::PointCloud<pcl::PointXYZ>::ConstPtr _s,
-				pcl::PointCloud<pcl::PointXYZ>::Ptr _p,
-				std::string _l,
-				graph_t *_g){
+			pcl::PointCloud<pcl::PointXYZ>::ConstPtr _s,
+			pcl::PointCloud<pcl::PointXYZ>::Ptr _p,
+			std::string _l,
+			pcl::PointCloud<pcl::PointXYZ>::Ptr _c,
+			std::string _cl,
+			plane _pl){
 		visualizer = _v;
 		sourceCloud = _s;
 		patchCloud = _p;
 		patchLabel = _l;
-		graph = _g;
+		reflectCloud = _c;
+		reflectLabel = _cl;
+		p = _pl;
 		color = std::make_tuple(255,0,0);
 		this->reset();
 	}
 
 	void reset(){
-		firstChosen = 0;
-		lastChosen = 0;
-		pointPickingDone = false;
-		firstPointPicked = false;
+		patchRadius = 5.0;
+		activePoint = -1;
+		pointPickingActive = false;
+		patchSelectionDone = false;
 		chosenPoints.clear();
-		WASDenabled = false;
 	}
 };
 
@@ -111,13 +117,11 @@ OFFData parseOff(const std::string &filename){
 		//std::cout<<x<<" "<<y<<" "<<z<<std::endl;
 		source_cloud->points.push_back(pcl::PointXYZ(x,y,z));
 	}
-	std::cout<<"Done"<<std::endl;
 	//Edges
 	//bool added[length][length];
 	std::vector<std::vector<bool> >added(length);
 	// for(auto x:added)
 	// 	x.resize(length);
-	puts("Done");
 	for(int i = 0;i<length;i++){
 		for(int j = 0; j<length;j++){
 			//std::cout<<i<<" "<<j<<std::endl;
@@ -221,32 +225,79 @@ void findShortestPath(graph_t &g,vertex_descriptor s,vertex_descriptor t,std::ve
 
 void pp_callback(const pcl::visualization::PointPickingEvent &e,void *in){
 	uiState *state = (uiState *)in; 
-	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(state->patchCloud,
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> red(state->patchCloud,
 		std::get<0>(state->color),
 		std::get<1>(state->color),
 		std::get<2>(state->color));
-	std::cout<<"Hello"<<std::endl;
-	if(state->pointPickingDone){
-		std::cout<<"Bye"<<std::endl;
+	if(state->pointPickingActive||state->patchSelectionDone){
 		return;
 	}
-	if(!state->firstPointPicked){
-		std::cout<<"Tarjan"<<std::endl;
-		state->chosenPoints.clear();
-		state->firstPointPicked = true;
-		state->firstChosen = e.getPointIndex();
-		state->lastChosen = e.getPointIndex();
-		state->patchCloud->points.push_back(state->sourceCloud->points[e.getPointIndex()]);
-		state->chosenPoints.push_back(e.getPointIndex());
+	else{
+		state->pointPickingActive = true;
+		state->patchCloud->points.clear();
+		state->activePoint = e.getPointIndex();
+		state->patchCloud->points.push_back(state->sourceCloud->points[state->activePoint]);
+		state->visualizer->updatePointCloud(state->patchCloud,red,state->patchLabel);		
+	}
+}
+
+void kb_callback(const pcl::visualization::KeyboardEvent &e,void *in){
+	uiState *state = (uiState *)in;
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> red(state->patchCloud,
+		std::get<0>(state->color),
+		std::get<1>(state->color),
+		std::get<2>(state->color));
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> yellow(state->reflectCloud,
+		0,
+		0,
+		255);
+	
+	if(state->pointPickingActive&&!state->patchSelectionDone&&e.keyDown()){
+		switch(e.getKeySym()[0]){
+			case 'w':
+			case 'W':
+				state->patchRadius += 1.0;
+				state->patchCloud->points.clear();
+				state->reflectCloud->points.clear();
+				for(const auto x:state->sourceCloud->points)
+					if(L2norm(x,state->sourceCloud->points[state->activePoint])<state->patchRadius){
+						state->patchCloud->points.push_back(x);
+						pcl::PointXYZ p(
+							std::get<0>(state->p),
+							std::get<1>(state->p),
+							std::get<2>(state->p)
+							);
+						state->reflectCloud->points.push_back(findReflection(p,x));
+					}
+				break;
+			case 's':
+			case 'S':
+				state->patchRadius -= 1.0;
+				state->patchCloud->points.clear();
+				state->reflectCloud->points.clear();
+				for(const auto x:state->sourceCloud->points)
+					if(L2norm(x,state->sourceCloud->points[state->activePoint])<state->patchRadius){
+						state->patchCloud->points.push_back(x);
+						pcl::PointXYZ p(
+							std::get<0>(state->p),
+							std::get<1>(state->p),
+							std::get<2>(state->p)
+							);
+						state->reflectCloud->points.push_back(findReflection(p,x));
+					}
+					break;		
+			case 'a':
+			case 'A':
+				state->patchSelectionDone = true;
+				break;
+			default:;
+		}
+		state->visualizer->updatePointCloud(state->patchCloud,red,state->patchLabel);
+		state->visualizer->updatePointCloud(state->reflectCloud,yellow,state->reflectLabel);
 	}
 	else{
-		int newPoint = e.getPointIndex();
-  		findShortestPath(*state->graph,state->lastChosen,newPoint,state->chosenPoints);
-  		for(const auto x:state->chosenPoints)
-			state->patchCloud->points.push_back(state->sourceCloud->points[x]);	
-  		state->lastChosen = newPoint;
+		return;
 	}
-	state->visualizer->updatePointCloud(state->patchCloud,single_color,state->patchLabel);
 }
 
 void addCloud(boost::shared_ptr<Visualizer> viewer,pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,const std::string &label){
@@ -260,11 +311,18 @@ void addRedCloud(boost::shared_ptr<Visualizer> viewer,pcl::PointCloud<pcl::Point
     viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, label.c_str());
 }
 
+void addBlueCloud(boost::shared_ptr<Visualizer> viewer,pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,const std::string &label){
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> single_color(cloud, 0, 0, 255);
+    viewer->addPointCloud<pcl::PointXYZ> (cloud, single_color, label.c_str());
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, label.c_str());
+}
+
 int main(){
 	OFFData d = parseOff("Full_face.off");
 	pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
 	kdtree.setInputCloud (d.source_cloud);
 	auto v = makeVisualizer();
+	plane p(9.327223,0.468285,0.717058);
 	/*
 	UI to select points
 	Just keep shift + selecting, 
@@ -273,12 +331,16 @@ int main(){
 	*/
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr patch(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PointCloud<pcl::PointXYZ>::Ptr reflect(new pcl::PointCloud<pcl::PointXYZ>());
 	const std::string patch_label("patch");
+	const std::string reflect_label("reflect");
   	graph_t g(d.edges.begin(),d.edges.end(),d.weights.begin(),d.nodes.size());
-	uiState state(v,d.source_cloud,patch,patch_label,&g);
+	uiState state(v,d.source_cloud,patch,patch_label,reflect,reflect_label,p);
   	v->registerPointPickingCallback(pp_callback,(void *)&state);
+  	v->registerKeyboardCallback(kb_callback,(void *)&state);
   	addCloud(v,d.source_cloud,"source");
   	addRedCloud(v,patch,patch_label);
+  	addBlueCloud(v,reflect,reflect_label);
 
 	while(!v->wasStopped()){
 		v->spinOnce(100);
